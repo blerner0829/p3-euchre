bd67110456046556ad4ef9efb9f0c00c9756d735
Benny Lerner <benjaminlerner@hotmail.com>
2023-02-08 15:55:30 -0500

    there it is

---
+++ pack.cpp
@@ -0,0 +1,47 @@
+// Project UID 1d9f47bfc76643019cfbf037641defe1
+#include "Pack.h"
+void Pack::reset() {
+  assert(false);
+  assert(next); // DELETEME: avoid error "private field is not used"
+}
+// EFFECTS: Initializes the Pack to be in the following standard order:
+  //          the cards of the lowest suit arranged from lowest rank to
+  //          highest rank, followed by the cards of the next lowest suit
+  //          in order from lowest to highest rank, and so on.
+  // NOTE: The standard order is the same as that in pack.in.
+  // NOTE: Do NOT use pack.in in your implementation of this function
+  Pack::Pack() {
+    
+   for (int i = 0; i < PACK_SIZE - 1; i++) {
+    if (cards[i + 1].get_suit() < cards[i].get_suit() || 
+        (cards[i + 1].get_suit() == cards[i].get_suit() && 
+         cards[i + 1].get_rank() < cards[i].get_rank())) {
+        cards[i] = cards[i + 1];
+    }
+    }
+  }
+  // REQUIRES: pack_input contains a representation of a Pack in the
+  //           format required by the project specification
+  // MODIFIES: pack_input
+  // EFFECTS: Initializes Pack by reading from pack_input.
+  Pack::Pack(std::istream& pack_input) {
+    assert(false);
+  }
+
+  // REQUIRES: cards remain in the Pack
+  // EFFECTS: Returns the next card in the pack and increments the next index
+  Pack::Card deal_one() {
+    assert(false);
+  }
+
+  // EFFECTS: Resets next index to first card in the Pack
+  void Pack::reset() {
+    assert(false);
+  }
+
+  // EFFECTS: Shuffles the Pack and resets the next index. This
+  //          performs an in shuffle seven times. See
+  //          https://en.wikipedia.org/wiki/In_shuffle.
+  void Pack::shuffle() {
+    assert(false);
+  }
