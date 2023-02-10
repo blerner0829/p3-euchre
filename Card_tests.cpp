bb6b5eb1f7d85a47c91dc58c5355a33236a47a12
Benny Lerner <benjaminlerner@hotmail.com>
2023-02-09 19:28:51 -0500

    TESTS

--- Card_tests.cpp
+++ Card_tests.cpp
@@ -13,6 +13,58 @@ TEST(test_card_ctor) {
     ASSERT_EQUAL(HEARTS, c.get_suit());
 }
 
-// Add more test cases here
+TEST(tests_suit_trump) {
+    Card c(JACK, HEARTS);
+    ASSERT_EQUAL(DIAMONDS, c.get_suit(DIAMONDS));
+    Card c1(JACK, DIAMONDS);
+    ASSERT_EQUAL(HEARTS, c1.get_suit(HEARTS));
+    Card c2(JACK, SPADES);
+    ASSERT_EQUAL(CLUBS, c2.get_suit(CLUBS));
+    Card c3(JACK, CLUBS);
+    ASSERT_EQUAL(SPADES, c3.get_suit(SPADES));
+    Card c4(TWO, DIAMONDS);
+    ASSERT_EQUAL(DIAMONDS, c4.get_suit(DIAMONDS));
+    Card c5(NINE, SPADES);
+    ASSERT_EQUAL(SPADES, c5.get_suit(DIAMONDS));
+}
+
+TEST(tests_is_face) {
+    Card c(JACK, HEARTS);
+    ASSERT_TRUE(c.is_face_or_ace());
+    Card c1(QUEEN, SPADES);
+    ASSERT_TRUE(c1.is_face_or_ace());
+    Card c2(KING, CLUBS);
+    ASSERT_TRUE(c2.is_face_or_ace());
+    Card c3(ACE, DIAMONDS);
+    ASSERT_TRUE(c3.is_face_or_ace()); 
+    Card c4(TWO, HEARTS);
+    ASSERT_FALSE(c4.is_face_or_ace());
+}
+
+TEST(tests_is_left_bower) {
+    Card c(JACK, HEARTS);
+    ASSERT_TRUE(c.is_left_bower(DIAMONDS));
+    ASSERT_FALSE(c.is_left_bower(SPADES));
+    Card c1(JACK, DIAMONDS);
+    ASSERT_TRUE(c1.is_left_bower(HEARTS));
+    Card c2(JACK, SPADES);
+    ASSERT_TRUE(c2.is_left_bower(CLUBS));
+    Card c3(JACK, CLUBS);
+    ASSERT_TRUE(c3.is_left_bower(SPADES));
+    Card c4(ACE, HEARTS);
+    ASSERT_FALSE(c4.is_left_bower(DIAMONDS));
+}
+
+TEST(tests_is_trump) {
+    Card c(ACE, SPADES);
+    ASSERT_TRUE(c.is_trump(SPADES));
+    ASSERT_FALSE(c.is_trump(CLUBS));
+    Card c1(JACK, HEARTS);
+    ASSERT_TRUE(c1.is_trump(HEARTS));
+    ASSERT_TRUE(c1.is_trump(DIAMONDS));
+    ASSERT_FALSE(c1.is_trump(SPADES));
+    Card c2(THREE, CLUBS);
+    ASSERT_TRUE(c2.is_trump(CLUBS));
+}
 
 TEST_MAIN()
